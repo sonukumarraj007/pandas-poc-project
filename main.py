ff56a1607a48b488d185e52c98bb919c5acaf6c9
sonukumarraj007 <sonukumarraj007@gmail.com>
2022-05-21 10:22:15 +0530

    code-updated-2

--- main.py
+++ main.py
@@ -1,29 +1,14 @@
 import datetime
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import my_lib as my_lib_obj
 import streamlit as st
 from streamlit_option_menu import option_menu
-import matplotlib.pyplot as plt
-import pandas as pd
-import my_lib as nf
-import time
-import seaborn as sns
-
-
-def covert_to_short_number(number):
-    return numerize.numerize(number, 2)
-
-
-def number_to_month(number):
-    number = int(number)
-    if number <= 12:
-        month_name_list = ['Jan', 'Feb', 'Mar', 'Apr', 'May',
-                           'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        return month_name_list[number-1]
-    else:
-        print('Wrong Input')
 
 
 def load_sales_data():
-    sales_data = pd.read_csv('./data/all_month_data.csv')
+    sales_data = pd.read_csv('./data/sales_data.csv')
     sales_data.dropna(axis="index", how="all", inplace=True)
     sales_data['Month'] = sales_data['Order Date'].str[0:2]
     sales_data.set_index('Order ID')
@@ -41,7 +26,7 @@ def monthly_sales_report():
     df = load_sales_data()
     df = df.groupby('Month').sum()
     df['Month Name'] = df.index
-    df['Month Name'] = df['Month Name'].apply(number_to_month)
+    df['Month Name'] = df['Month Name'].apply(my_lib_obj.number_to_month)
 
     fig = plt.figure(figsize=(8, 4))
     # sns.set_style('darkgrid')
@@ -71,7 +56,7 @@ def monthly_sales_report_line_chart():
     df = load_sales_data()
     df = df.groupby('Month').sum()
     df['Month Name'] = df.index
-    df['Month Name'] = df['Month Name'].apply(number_to_month)
+    df['Month Name'] = df['Month Name'].apply(my_lib_obj.number_to_month)
 
     fig = plt.figure(figsize=(8, 4))
     # sns.set_style('darkgrid')
@@ -111,11 +96,9 @@ with st.sidebar:
         options=[
             "Sales Report",
             "Covid 19 Report",
-            "Zomato",
-            "Test 1",
-            "Test 2"
+            "Zomato"
         ],
-        icons=["", "", "", "", ""],
+        icons=["", "", ""],
         menu_icon="cast",
         default_index=0,
     )
@@ -154,80 +137,3 @@ if selected == "Covid 19 Report":
 # zomato report
 if selected == "Zomato":
     st.header("working on...")
-
-if selected == "Test 1":
-    report_form = st.form(key="annotation2")
-    with report_form:
-        row_1 = st.columns(4)
-        start_date = row_1[0].date_input("Start Date:")
-        end_date = row_1[1].date_input("End Date:")
-
-        nifty_price = 17122
-        ce_strike = nf.ce_strike_list(nifty_price)
-        pe_strike = nf.pe_strike_list(nifty_price)
-
-        ce_strike_list = row_1[2].selectbox("CE Strike :", ce_strike)
-        pe_strike_list = row_1[3].selectbox("PE Strike :", pe_strike)
-
-        row_2 = st.columns(4)
-
-        entry_time = row_2[0].time_input(
-            "Select Entry Time : ", datetime.time(9, 15)
-        )
-        exit_time = row_2[1].time_input(
-            "Select Exit Time : ", datetime.time(9, 15))
-
-        report_form_submitted = row_2[3].form_submit_button(label="Submit")
-
-    if report_form_submitted:
-        st.success("Your report loaded successfully!")
-        st.write(f"Your start date : ({start_date})")
-        st.write(f"Your end date : ({end_date})")
-        st.write("Your CE selected strike is: ", ce_strike_list)
-        st.write("Your PE selected strike is: ", pe_strike_list)
-        st.write("your entry time : ", entry_time)
-        st.write("your exit time : ", exit_time)
-
-if selected == "Test 2":
-    uploaded_file = st.file_uploader("Upload CSV", type=".csv")
-    if uploaded_file:
-        df = pd.read_csv(uploaded_file)
-
-        st.table(df)
-        st.write(df.describe().transpose())
-
-        fig1, ax1 = plt.subplots()
-        plt.ylabel("No. of Days")
-        plt.title("Index Move Histogram")
-        ax1.hist(df["Move"], bins=20, rwidth=0.6)
-        st.pyplot(fig1)
-
-        fig2, ax2 = plt.subplots()
-        plt.ylabel("No. of Days")
-        plt.title("Index Change % Histogram")
-        ax2.hist(df["Change %"], bins=20, rwidth=0.6)
-        st.pyplot(fig2)
-
-        st.header("Index Move line Chart")
-        st.line_chart(df["Move"])
-
-        col1, col2 = st.columns(2)
-        with col1:
-            st.header("Top 5 Down Move")
-            top_5_small_data = df.nsmallest(5, "Move")
-            st.write("1st : ", top_5_small_data["Move"].iloc[0])
-            st.write("2nd : ", top_5_small_data["Move"].iloc[1])
-            st.write("3rd : ", top_5_small_data["Move"].iloc[2])
-            st.write("4th : ", top_5_small_data["Move"].iloc[3])
-            st.write("5th : ", top_5_small_data["Move"].iloc[4])
-
-        with col2:
-            st.header("Top 5 Up Move")
-
-            top_5_larg_data = df.nlargest(5, "Move")
-
-            st.write("1st : ", top_5_larg_data["Move"].iloc[0])
-            st.write("2nd : ", top_5_larg_data["Move"].iloc[1])
-            st.write("3rd : ", top_5_larg_data["Move"].iloc[2])
-            st.write("4th : ", top_5_larg_data["Move"].iloc[3])
-            st.write("5th : ", top_5_larg_data["Move"].iloc[4])
--- my_lib.py
+++ my_lib.py
@@ -1,44 +1,17 @@
-
-# get current nifty strike
-def current_strike(price):
-    res = price % 50
-    if res < 25:
-        return price - res
-    elif res > 25:
-        return price-res+50
-
-
-# get CE strike dropdwon list
-def ce_strike_list(price):
-    ce_strike = current_strike(price)
-    ce_strike_list = []
-    append_ce_string = 'CE'
-
-    for i in range(1, 11):
-        ce_strike_list.append(str(ce_strike + (i * 50)) + append_ce_string)
-
-    return ce_strike_list
-
-
-# get PE strike dropdwon list
-def pe_strike_list(price):
-    pe_strike = current_strike(price)
-    pe_strike_list = []
-    append_pee_string = 'PE'
-
-    for i in range(1, 11):
-        pe_strike_list.append(str(pe_strike - (i * 50)) + append_pee_string)
-
-    return pe_strike_list
-
-
 def day_name_list():
     days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
     return days
 
 
-def get_symbol(symbol):
-    if symbol == 'BANKNIFTY':
-        return 'NSE:NIFTYBANK-INDEX'
-    elif symbol == 'NIFTY50':
-        return 'NSE:NIFTY50-INDEX'
+def covert_to_short_number(number):
+    return numerize.numerize(number, 2)
+
+
+def number_to_month(number):
+    number = int(number)
+    if number <= 12:
+        month_name_list = ['Jan', 'Feb', 'Mar', 'Apr', 'May',
+                           'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        return month_name_list[number-1]
+    else:
+        return 'Wrong Input'
